6a691e3ed78d794124d6d06fe89e420c067226bc
Ian Teda <ian@teda.id.au>
2019-07-07 09:14:38 +1000

    Ember Bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+
+    /**
+     * EMBER BOOTSTRAP
+     * https://www.ember-bootstrap.com
+     * ember install ember-bootstrap
+     */
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
